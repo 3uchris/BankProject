9526fd89a93445efffa09688c9ff724b66d4ce7e
Chris Wu <yiming19@ualberta.ca>
2022-08-22 18:29:40 -0600

    added unit test for hashing password

---
+++ util/password_test.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+
+	HashedPassword, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, HashedPassword)
+
+	err = CheckPassword(password, HashedPassword)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(6)
+	err = CheckPassword(wrongPassword, HashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+}
