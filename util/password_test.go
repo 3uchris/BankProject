409bf54d3fe605b02d4f85b967e85d7da2d1a23e
Chris Wu <yiming19@ualberta.ca>
2022-08-22 19:42:07 -0600

    stored users' password using bcrypt securely and implemented API to create a new user

--- util/password_test.go
+++ util/password_test.go
@@ -10,15 +10,20 @@ import (
 func TestPassword(t *testing.T) {
 	password := RandomString(6)
 
-	HashedPassword, err := HashedPassword(password)
+	hashedPassword1, err := HashedPassword(password)
 	require.NoError(t, err)
 	require.NotEmpty(t, HashedPassword)
 
-	err = CheckPassword(password, HashedPassword)
+	err = CheckPassword(password, hashedPassword1)
 	require.NoError(t, err)
 
 	wrongPassword := RandomString(6)
-	err = CheckPassword(wrongPassword, HashedPassword)
+	err = CheckPassword(wrongPassword, hashedPassword1)
 	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
 
+	hashedPassword2, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+
+	require.NotEqual(t, hashedPassword1, hashedPassword2)
 }
